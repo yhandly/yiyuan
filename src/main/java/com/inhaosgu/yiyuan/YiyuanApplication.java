9e4b163df39874df419b58e687541b65b56e0e52
Administrator <123>
2020-11-02 16:23:37 +0800

    初始项目

---
+++ src/main/java/com/inhaosgu/yiyuan/YiyuanApplication.java
@@ -0,0 +1,13 @@
+package com.inhaosgu.yiyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiyuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiyuanApplication.class, args);
+    }
+
+}
